589c06df69faeb49e5d23734117e512d1611fbeb
Imran Hossen <imranbappy.official@gmail.com>
2021-02-03 22:36:39 +0600

    fast commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
-function App() {
+const App = () => {
+
+
+  const data = [
+    { name: 'Page A', uv: 400, },
+    { name: 'Page A', uv: 100, },
+    { name: 'Page A', uv: 250, },
+    { name: 'Page A', uv: 500, },
+    { name: 'Page A', uv: 50, },
+    { name: 'Page A', uv: 300, },
+  ];
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Comming chart.....</h2><br /><br />
+        <LineChart width={800} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
       </header>
     </div>
   );
